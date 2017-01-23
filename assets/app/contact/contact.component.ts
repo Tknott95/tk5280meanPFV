1fda1d3d64f5474801229f47dd7fd193c3227297
tknott95 <tknott95@hotmail.com>
2017-01-22 23:41:18 -0700

    CONTACTCOOMPONENT-LULZYLUZLZLULZ

--- assets/app/contact/contact.component.ts
+++ assets/app/contact/contact.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'tk-contact-page',
     templateUrl: './contact.component.html',
-    styleUrls: ['./contact.component.css']
+    styleUrls: ['contact.component.scss']
 })
 
 export class ContactPageComponent {
